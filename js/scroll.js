943b6b938847f283bce2eaefe68cf4ff9b10e605
mstoiber <contact@mstoiber.com>
2014-05-11 19:36:12 +0200

    Added favicon, added Frontpage Buttons for Portfolio and Blog, changed header fonts, added logo, added progressive jpeg for frontpage.jpg

--- js/scroll.js
+++ js/scroll.js
@@ -1,22 +1,40 @@
 $(document).ready(function (){
-    $("#a").click(function (){
-        //$(this).animate(function(){
-            $('html, body').animate({
-                scrollTop: $("#aboutme").offset().top
-            }, 1500);
-        //});
+
+    // =====
+    // Frontpage Buttons
+    // =====
+
+    // About Me Button
+    $("#a").click(function (e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#aboutme").offset().top
+        }, 1500);
     });
+
+    // Portfolio Button
+    $("#p").click(function (e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#portfolio").offset().top
+        }, 1500);
+    });
+
+    // =====
+    // Scroll To Top Button
+    // =====
     //Check to see if the window is top if not then display button
     $(window).scroll(function(){
         if ($(this).scrollTop() > 300) {
-            $('.scrollToTop').fadeIn();
+            $('.btn-scrollToTop').fadeIn();
         } else {
-            $('.scrollToTop').fadeOut();
+            $('.btn-scrollToTop').fadeOut();
         }
     });
     
     //Click event to scroll to top
-    $('.scrollToTop').click(function(){
+    $('.btn-scrollToTop').click(function(e){
+        e.preventDefault();
         $('html, body').animate({scrollTop : 0},800);
         return false;
     });
