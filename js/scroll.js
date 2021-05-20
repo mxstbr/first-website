6ef3e28d225648eda96af886b95919dca6a7919e
Max Stoiber <contact@mxstbr.com>
2021-05-20 21:11:43 +0200

    Commit latest version

--- js/scroll.js
+++ js/scroll.js
@@ -1,47 +1,17 @@
+window.touchCheck = function() {
+    return Modernizr.touch;
+};
+window.mobileCheck = function(){
+    return $(window).width()<=mobileThreshold;
+};
+window.mobileThreshold=767;
+
 $(document).ready(function (){
 
-    // =====
-    // Frontpage Buttons
-    // =====
-
-    // About Me Button
-    $("#a").click(function (e){
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $("#aboutme").offset().top
-        }, 1500);
-    });
-
-    // Portfolio Button
-    $("#p").click(function (e){
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $("#portfolio").offset().top
-        }, 1500);
-    });
-
-    // =====
-    // Scroll To Top Button
-    // =====
-    //Check to see if the window is top if not then display button
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 300) {
-            $('.btn-scrollToTop').fadeIn();
-        } else {
-            $('.btn-scrollToTop').fadeOut();
-        }
-    });
+    if(mobileCheck() || touchCheck()){
+        return 0;
+    } else {
+        var s = skrollr.init();
+    }
     
-    //Click event to scroll to top
-    $('.btn-scrollToTop').click(function(e){
-        e.preventDefault();
-        $('html, body').animate({scrollTop : 0},800);
-        return false;
-    });
-
-    $('.title').addClass('animated fadeInUp');
-
-    setTimeout(function () {
-        $('.subtitle').show().addClass('animated fadeInUp');}, 300
-    );
 });
